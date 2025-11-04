3cb53bf395f16b84728c68a5ed6911eb89f52cb6
Juan Duarte <12345678+username@users.noreply.github.com>
2025-11-04 08:06:19 +0200

    Blank State

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,11 @@
 
 void setup() {
   Serial.begin(115200);
-  setup_mux();
-  setup_adc();
+
 }
 
 void loop() {
-  // mux_sequence_runner();
-  delay(10);
-  all_mux();
-  // loop_adc();
+
   //all_mux_odd_even();
   //all_mux_ramping();
   // all_mux_nonblocking();
