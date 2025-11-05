526ecb66647a5a2fc54ca31a53ea27959712fec6
Juan Duarte <12345678+username@users.noreply.github.com>
2025-11-05 12:08:46 +0200

    Water Level Sensor Read Example

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,20 @@
 #include "mux.h"
 #include "adc.h"
 
+#define LED_BUILTIN 8
+
 void setup() {
   Serial.begin(115200);
-
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
+  int x = analogRead(A0);
+  Serial.printf("Analog read A0: %d\n", x);
+  delay(250);
+  if(x < 1000){
+    digitalWrite(LED_BUILTIN, LOW);  
+  } else{
+    digitalWrite(LED_BUILTIN, HIGH);  
+  }
 }
